384b3f5980be0f9ffe0161775e75edac24145e10
hschickdevs <64290193+hschickdevs@users.noreply.github.com>
2022-04-08 00:34:18 -0500

    Telegram 2FA handler improvements

--- ibeam/src/two_fa_handlers/telegram_msg_handler.py
+++ ibeam/src/two_fa_handlers/telegram_msg_handler.py
@@ -17,6 +17,8 @@ TELEGRAM_ADMIN_IDS = os.environ.get('IBEAM_TELEGRAM_ADMIN_IDS', None)
 """A no-space comma-separated list of telegram user admin ids (e.g. 123456789,987654321)"""
 
 BASE_PROMPT_MSG = "The IBKR Gateway requires a new 2FA code."
+MAXIMUM_RETRIES = 5
+AWAIT_TIMEOUT = 43200  # 12 hours
 
 
 class TelegramMessageHandler(TwoFaHandler):
@@ -30,25 +32,52 @@ class TelegramMessageHandler(TwoFaHandler):
         self.alert_admins(message)
         return self.await_2fa_code()
 
+    def http_request(self, _type: str, url: str, data: dict = None, _try: int = 1) -> dict:
+        """Attempt to make an http request to the given url. If the request fails, retry with recursion"""
+        if _try > MAXIMUM_RETRIES:
+            raise ConnectionError(f"Could not connect to Telegram API after 5 attempts with url: {url}")
+
+        if _type == 'GET':
+            r = requests.get(url, params=data if data is not None else {})
+        elif _type == 'POST':
+            r = requests.post(url, json=data if data is not None else {})
+        else:
+            raise ValueError(f"Invalid http request type: {_type} (must be GET or POST)")
+
+        if r.status_code == 200:
+            return r.json()
+        elif r.status_code == 500:
+            # Specific condition for the Google Cloud service being used - 10 seconds delay before retrying
+            _LOGGER.error(f"Internal server error (500) thrown when attempting to {_type} from Telegram API - "
+                          f"10 second retry delay")
+            time.sleep(10)
+        else:
+            # General condition for other errors - 1 second delay before retrying
+            _LOGGER.error(f"Telegram API returned an error code: {r.status_code, r.text} - 3 second retry delay")
+            time.sleep(1)
+
+        return self.http_request(_type, url, data, _try + 1)
+
     def alert_admins(self, message):
-        """For each admin in ADMIN_IDS, send a post request using urllib3 to alert them of the 2FA code"""
+        """For each admin in ADMIN_IDS, send a post request using urllib3 to alert them of the 2FA code request"""
         for admin_id in TELEGRAM_ADMIN_IDS.split(","):
-            try:
-                requests.post(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage",
-                              json={'chat_id': admin_id, 'text': message})
-            except Exception as exc:
-                print(f"TELEGRAM FAULT: Could not post alert to telegram\nError Code: {exc}")
+            self.http_request('POST', f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage',
+                              {'chat_id': admin_id, 'text': message})
 
     def await_2fa_code(self) -> str:
+        """Awaits the 2FA code response from either of the admins - provided in os.getenv('IBEAM_TELEGRAM_ADMIN_IDS')"""
         start_timestamp = time.time()
-        # _LOGGER.info("Waiting for user response to 2FA prompt...")
         failed_attempts = []
         while True:
-            updates = requests.get(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/getUpdates").json()['result']
+            if time.time() >= (AWAIT_TIMEOUT + start_timestamp):
+                raise TimeoutError(f"Timed out after {AWAIT_TIMEOUT} seconds waiting for 2FA code response")
+
+            updates = self.http_request('GET', f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/getUpdates')['result']
             for update in updates:
                 upd_id = update['update_id']
                 timestamp = update['message']['date']
                 message = update['message']['text']
+
                 if (upd_id not in failed_attempts) and (timestamp > start_timestamp) and ("code:" in message.lower()):
                     try:
                         code = message.lower().strip().replace(" ", "").split("code:")[1]
@@ -59,8 +88,3 @@ class TelegramMessageHandler(TwoFaHandler):
                         failed_attempts.append(upd_id)
                         self.alert_admins("Could not parse code from message.\nPlease use the following format:"
                                           "\nCODE: 12345678")
-            time.sleep(1)
-
-
-if __name__ == "__main__":
-    TelegramMessageHandler().get_two_fa_code()
